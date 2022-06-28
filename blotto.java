78ee12f78fced627a09174ccda5960feef2ba8ca
Vivian Ye <100059668+vvaye@users.noreply.github.com>
2022-06-27 23:12:24 -0700

    Create blotto.java

---
+++ blotto.java
@@ -0,0 +1,195 @@
+package main;
+import java.util.*;
+
+public class blotto {
+    public static void main(String[] args) {
+        System.out.println(findAnswer(8) + " total");
+    }
+
+    //generate dividing 50 people into 5 teams
+    public static int findAnswer(int total) {
+        int people = total;
+        int counter = 0;
+        String[][] finalScores = new String[2][92378];
+        String[][] top5 = new String[2][5];
+        for (int i = 0; i <= total; i++) {
+            int[] team1 = new int[10];
+            team1[0] = i;
+            total = total- i;
+            for (int j = 0; j <= total; j++) {
+                team1[1] = j;
+                total = total - j;
+                for (int k = 0; k <= total; k++) {
+                    team1[2] = k;
+                    total = total - k;
+                    for (int l = 0; l <= total; l++) {
+                        team1[3] = l;
+                        total = total - l;
+                        for (int m = 0; m <= total; m++) {
+                            team1[4] = m;
+                            total = total - m;
+                            for (int n = 0; n <= total; n++) {
+                                team1[5] = n;
+                                total = total - n;
+                                for (int o = 0; o <= total; o++) {
+                                    team1[6] = o;
+                                    total = total - o;
+                                    for (int p = 0; p <= total; p++) {
+                                        team1[7] = p;
+                                        total = total - p;
+                                        for (int q = 0; q <= total; q++) {
+                                            team1[8] = q;
+                                            total = total - q;
+                                            //code specifically for the last tower in the array
+                                            team1[9] = total;
+                                            String first = new String("Team: [");
+                                            for (int r = 0; r < 10; r++) {
+                                                if (r == 9) {
+                                                    first += team1[r];
+                                                } else {
+                                                    first += (team1[r] + ",");
+                                                }
+                                            }
+                                            //System.out.println(first + "]");
+                                            finalScores[0][counter] = first;
+                                            finalScores[1][counter] = String.valueOf(getTeam(people, team1));
+                                            //prints array
+
+                                            /*System.out.print("\n[");
+                                            for (int r = 0; r < 10; r++) {
+                                                if (r == 9) {
+                                                    System.out.print(array[r]);
+                                                } else {
+                                                    System.out.print(array[r] + ", ");
+                                                }
+                                            }
+                                            System.out.print("]");*/
+                                            counter++;
+                                            total = total + q;
+                                        }
+                                        total = total + p;
+                                    }
+                                    total = total + o;
+                                }
+                                total = total + n;
+                            }
+                            total = total + m;
+                        }
+                        total = total + l;
+                    }
+                    total = total + k;
+                }
+                total = total + j;
+            }
+            total = total + i;
+        }
+
+        for (int i = 0; i < 15; i++) {
+
+            System.out.println(finalScores[0][i] + "] Score: " + finalScores[1][i]);
+
+        }
+        return counter;
+    }
+
+    public static double getTeam(int total, int[] team1) {
+        int counter = 0;
+        double team1Score = 0;
+        for (int i = 0; i <= total; i++) {
+            int[] team2 = new int[10];
+            team2[0] = i;
+            total = total- i;
+            for (int j = 0; j <= total; j++) {
+                team2[1] = j;
+                total = total - j;
+                for (int k = 0; k <= total; k++) {
+                    team2[2] = k;
+                    total = total - k;
+                    for (int l = 0; l <= total; l++) {
+                        team2[3] = l;
+                        total = total - l;
+                        for (int m = 0; m <= total; m++) {
+                            team2[4] = m;
+                            total = total - m;
+                            for (int n = 0; n <= total; n++) {
+                                team2[5] = n;
+                                total = total - n;
+                                for (int o = 0; o <= total; o++) {
+                                    team2[6] = o;
+                                    total = total - o;
+                                    for (int p = 0; p <= total; p++) {
+                                        team2[7] = p;
+                                        total = total - p;
+                                        for (int q = 0; q <= total; q++) {
+                                            team2[8] = q;
+                                            total = total - q;
+                                            //code specifically for the last tower in the array
+                                            team2[9] = total;
+                                            String second = new String("\nTeam 2 is: [");
+                                            for (int r = 0; r < 10; r++) {
+                                                if (r == 9) {
+                                                    second += team2[r];
+                                                } else {
+                                                    second += (team2[r] + ",");
+                                                }
+                                            }
+                                            //System.out.println(second + "]");
+                                            //System.out.println(getPoints(team1, team2));
+                                            team1Score = team1Score + getPoints(team1, team2);
+                                            //prints array
+
+                                            /*System.out.print("\n[");
+                                            for (int r = 0; r < 10; r++) {
+                                                if (r == 9) {
+                                                    System.out.print(array[r]);
+                                                } else {
+                                                    System.out.print(array[r] + ", ");
+                                                }
+                                            }
+                                            System.out.print("]");*/
+                                            counter++;
+                                            total = total + q;
+                                        }
+                                        total = total + p;
+                                    }
+                                    total = total + o;
+                                }
+                                total = total + n;
+                            }
+                            total = total + m;
+                        }
+                        total = total + l;
+                    }
+                    total = total + k;
+                }
+                total = total + j;
+            }
+            total = total + i;
+        }
+        return team1Score;
+    }
+
+    public static double getPoints (int[] team1, int[] team2) {
+        double points1 = 0;
+        double points2 = 0;
+        for (int i = 0; i < team1.length; i++) {
+            if (team1[i] > team2[i]) {
+                points1 = points1 + 1;
+            } else if (team1[i] == team2[i]) {
+                points1 = points1 + 0.5;
+                points2 = points2 + 0.5;
+            } else {
+                points2 = points2 + 1;
+            }
+        }
+        double pointsGained = 0;
+        if (points1 > points2) {
+            pointsGained = 1;
+        } else if (points1 == points2) {
+            pointsGained = 0.5;
+        } else {
+            pointsGained = 0;
+        }
+        return pointsGained;
+    }
+}
